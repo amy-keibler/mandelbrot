3fef8d3a5d68b99774cb5e4c3a7438cb3bf370e7
Jane Keibler <wanderingelf14@gmail.com>
2016-07-19 22:12:13 -0400

    migrated and refactored C++ implementation
    Migrated escape time algorithm.
    Removed unnecessary tests and testing code. (Bounds didn't make sense)

--- src/Mandelbrot.hs
+++ src/Mandelbrot.hs
@@ -2,5 +2,18 @@ module Mandelbrot (numIterations) where
 
 import Data.Complex
 
-numIterations :: Complex a -> Maybe Integer
-numIterations _ = Nothing
+maxIterations :: Integral a => a
+maxIterations = 256
+
+radius :: RealFloat a => a
+radius = 3
+
+numIterations :: RealFloat a => Complex a -> Int
+numIterations c = length $ take maxIterations $ takeWhile (lessThanRadius radius) $ zSeries c
+
+lessThanRadius :: (Floating a, Ord a) => a -> Complex a -> Bool
+lessThanRadius r z = realPart z ^ (2 :: Integer) + imagPart z ^ (2 :: Integer) < r ^ (2 :: Integer)
+
+zSeries :: RealFloat a => Complex a -> [Complex a]
+zSeries c = iterate nextZ (0 :+ 0)
+  where nextZ z = z ^ (2 :: Integer) + c
--- test/MandelbrotSpec.hs
+++ test/MandelbrotSpec.hs
@@ -2,11 +2,9 @@ module MandelbrotSpec (main, spec) where
 
 import Test.Hspec
 import Test.QuickCheck
-import System.Random
 
 import Mandelbrot (numIterations)
 import Data.Complex
-import Data.List (genericLength)
 
 main :: IO ()
 main = hspec spec
@@ -14,32 +12,12 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "numIterations" $ do
-    it "is undefined for numbers outside the range x [-2.5, 1], y [-1, 1]" $ do
-      forAll (oneof [genBelowRange, genAboveRange]) $ \a -> numIterations (a :: Complex Double) `shouldBe` Nothing
-    it "it matches the cpp project definition" $ do
-      forAll genInRange $ \a -> numIterations (a :: Complex Double) `shouldBe` Just (cppMandelbrot a)
+    it "it matches the cpp project definition" $ property $
+      \a -> numIterations (a :: Complex Double) `shouldBe` cppMandelbrot a
 
 
-genInRange :: (Fractional a, Random a) => Gen (Complex a)
-genInRange = do
-  x <- choose (-2.5, 1)
-  y <- choose (-1, 1)
-  return (x :+ y)
-
-genAboveRange :: Gen (Complex Double)
-genAboveRange = do
-  x <- choose (1.001, 1000)
-  y <- choose (1.001, 1000)
-  return (x :+ y)
-
-genBelowRange :: Gen (Complex Double)
-genBelowRange = do
-  x <- choose (-1000, -2.499)
-  y <- choose (-1000, -1.999)
-  return (x :+ y)
-
-cppMandelbrot :: Complex Double -> Integer
-cppMandelbrot c = genericLength $ take 256 $ takeWhile (lessThanRadius 3) $ iterate (nextZ c) (0.0 :+ 0.0)
+cppMandelbrot :: Complex Double -> Int
+cppMandelbrot c = length $ take 256 $ takeWhile (lessThanRadius 3) $ iterate (nextZ c) (0.0 :+ 0.0)
 
 nextZ :: Complex Double -> Complex Double -> Complex Double
 nextZ c z = z * z + c
